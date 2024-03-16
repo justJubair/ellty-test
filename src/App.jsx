edd394d57a90377e457439ff9bffb97c08972ff1
Jubair <jubair.ahmed2838@gmail.com>
2024-03-16 22:11:36 +0600

    Add: done button functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,53 @@
 import { useState } from "react";
 import "./App.Module.css";
 
-
-const App=()=> {
+const App = () => {
   const [selectAll, setSelectAll] = useState(false);
-    // state to hold the values of multiple checkboxes
-    const [checkboxValues, setCheckboxValues] = useState({
-      checkbox1: false,
-      checkbox2: false,
-      checkbox3: false,
-      checkbox4: false,
-    })
-  
+
+  // state to hold the values of multiple checkboxes
+  const [checkboxValues, setCheckboxValues] = useState({
+    checkbox1: false,
+    checkbox2: false,
+    checkbox3: false,
+    checkbox4: false,
+  });
 
   // handle select all checkboxs
-  const handleSelectAllChange = (e)=>{
-    const {checked} = e.target;
+  const handleSelectAllChange = (e) => {
+    const { checked } = e.target;
     setSelectAll(checked);
 
     const updatedCheckboxValues = {};
-    for (const key in checkboxValues){
+    for (const key in checkboxValues) {
       updatedCheckboxValues[key] = checked;
     }
-    setCheckboxValues(updatedCheckboxValues)
-  }
-
+    setCheckboxValues(updatedCheckboxValues);
+  };
 
   // handle checkbox change
-  const handleCheckboxChange = (e)=>{
-    const {name, checked} = e.target;
+  const handleCheckboxChange = (e) => {
+    const { name, checked } = e.target;
     setCheckboxValues({
-      ...checkboxValues, [name]: checked,
-    })
+      ...checkboxValues,
+      [name]: checked,
+    });
+  };
+
+  const handleUnselectAll = ()=>{
+    const updatedCheckboxValues = {};
+    for (const key in checkboxValues) {
+      updatedCheckboxValues[key] = false;
+    }
+    setCheckboxValues(updatedCheckboxValues);
+    setSelectAll(false)
   }
 
   // handle form submission
-  const handlePages = (e)=>{
-    e.preventDefault()
-   
-    console.log(`Checkbox values`, checkboxValues)
-  }
+  const handlePages = (e) => {
+    e.preventDefault();
+
+    
+  };
   return (
     <>
       <div className="main">
@@ -49,59 +57,111 @@ const App=()=> {
             <p>All pages</p>
             <form>
               <label className="checkbox-container">
-                <input type="checkbox"  checked={selectAll} onChange={handleSelectAllChange} />
+                <input
+                  type="checkbox"
+                  checked={selectAll}
+                  onChange={handleSelectAllChange}
+                />
                 <span className="checkmark"></span>
               </label>
             </form>
           </div>
 
-          <form onSubmit={handlePages} >
+          <form onSubmit={handlePages}>
             <div className="row-collection">
-            <div className="row" style={checkboxValues.checkbox1 ? { textDecoration: "line-through" } : {}}>
-              <p>page 1</p>
+              <div
+                className="row"
+                style={
+                  checkboxValues.checkbox1
+                    ? { textDecoration: "line-through" }
+                    : {}
+                }
+              >
+                <p>page 1</p>
 
-              <label className="checkbox-container">
-                <input type="checkbox" name="checkbox1" checked={checkboxValues.checkbox1} onChange={handleCheckboxChange}  />
-                <span className="checkmark"></span>
-              </label>
+                <label className="checkbox-container">
+                  <input
+                    type="checkbox"
+                    name="checkbox1"
+                    checked={checkboxValues.checkbox1}
+                    onChange={handleCheckboxChange}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+              </div>
+
+              <div
+                className="row"
+                style={
+                  checkboxValues.checkbox2
+                    ? { textDecoration: "line-through" }
+                    : {}
+                }
+              >
+                <p>page 2</p>
+
+                <label className="checkbox-container">
+                  <input
+                    type="checkbox"
+                    name="checkbox2"
+                    checked={checkboxValues.checkbox2}
+                    onChange={handleCheckboxChange}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+              </div>
+
+              <div
+                className="row"
+                style={
+                  checkboxValues.checkbox3
+                    ? { textDecoration: "line-through" }
+                    : {}
+                }
+              >
+                <p>page 3</p>
+
+                <label className="checkbox-container">
+                  <input
+                    type="checkbox"
+                    name="checkbox3"
+                    checked={checkboxValues.checkbox3}
+                    onChange={handleCheckboxChange}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+              </div>
+
+              <div
+                className="row"
+                style={
+                  checkboxValues.checkbox4
+                    ? { textDecoration: "line-through" }
+                    : {}
+                }
+              >
+                <p>page 4</p>
+
+                <label className="checkbox-container">
+                  <input
+                    type="checkbox"
+                    name="checkbox4"
+                    checked={checkboxValues.checkbox4}
+                    onChange={handleCheckboxChange}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+              </div>
             </div>
 
-            <div className="row" style={checkboxValues.checkbox2 ? { textDecoration: "line-through" } : {}}>
-              <p>page 2</p>
-
-              <label className="checkbox-container">
-                <input type="checkbox" name="checkbox2" checked={checkboxValues.checkbox2} onChange={handleCheckboxChange}/>
-                <span className="checkmark"></span>
-              </label>
-            </div>
-
-            <div className="row" style={checkboxValues.checkbox3 ? { textDecoration: "line-through" } : {}}>
-              <p>page 3</p>
-
-              <label className="checkbox-container">
-                <input type="checkbox" name="checkbox3" checked={checkboxValues.checkbox3} onChange={handleCheckboxChange} />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-
-            <div className="row" style={checkboxValues.checkbox4 ? { textDecoration: "line-through" } : {}}>
-              <p>page 4</p>
-
-              <label className="checkbox-container">
-                <input type="checkbox" name="checkbox4" checked={checkboxValues.checkbox4} onChange={handleCheckboxChange} />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            </div>
-            
-            <button  type="submit" className={`btn`}>Done</button>
+            <button onClick={handleUnselectAll} type="submit" className={`btn`}>
+              Done
+            </button>
           </form>
-
-         
         </div>
       </div>
     </>
   );
-}
+};
 
 export default App;

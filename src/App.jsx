ca845d50d7a1b7821c1daa5281d5bd66fb06637a
Jubair <jubair.ahmed2838@gmail.com>
2024-03-16 20:20:22 +0600

    Add: initial component design done

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,62 @@ function App() {
       <div className="main">
         <div className="container">
           {/* all pages */}
-          <div className="row">
+          <div className="all-pages">
             <p>All pages</p>
             <form>
               <label className="checkbox-container">
-                <input type="checkbox" name="" id="" />
+                <input type="checkbox" name="all-pages" id="" />
                 <span className="checkmark"></span>
               </label>
+              
             </form>
           </div>
+
+          <div className="row">
+            <p>page 1</p>
+            <form>
+              <label className="checkbox-container">
+                <input type="checkbox" name="all-pages" id="" />
+                <span className="checkmark"></span>
+              </label>
+              
+            </form>
+          </div>
+
+          <div className="row">
+            <p>page 2</p>
+            <form>
+              <label className="checkbox-container">
+                <input type="checkbox" name="all-pages" id="" />
+                <span className="checkmark"></span>
+              </label>
+              
+            </form>
+          </div>
+
+          <div className="row">
+            <p>page 3</p>
+            <form>
+              <label className="checkbox-container">
+                <input type="checkbox" name="all-pages" id="" />
+                <span className="checkmark"></span>
+              </label>
+              
+            </form>
+          </div>
+
+          <div className="row">
+            <p>page 4</p>
+            <form>
+              <label className="checkbox-container">
+                <input type="checkbox" name="all-pages" id="" />
+                <span className="checkmark"></span>
+              </label>
+              
+            </form>
+          </div>
+           
+           <button className="btn">Done</button>
         </div>
       </div>
     </>

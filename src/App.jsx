12f2954b3bb357dad257622f3dc9914645968372
Jubair <jubair.ahmed2838@gmail.com>
2024-03-16 19:45:52 +0600

    Add: checkbox added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-
+import "./App.Module.css";
 function App() {
-
-
   return (
     <>
-     
-      <h1>Vite + React</h1>
-     
-     
+      <div className="main">
+        <div className="container">
+          {/* all pages */}
+          <div className="row">
+            <p>All pages</p>
+            <form>
+              <label className="checkbox-container">
+                <input type="checkbox" name="" id="" />
+                <span className="checkmark"></span>
+              </label>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

4f26a9ead81f457e62ac608b3738998a9d2cfe87
Jubair <jubair.ahmed2838@gmail.com>
2024-03-16 21:52:42 +0600

    Add: line through style added in checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
 import "./App.Module.css";
-function App() {
+
+
+const App=()=> {
+  const [selectAll, setSelectAll] = useState(false);
+    // state to hold the values of multiple checkboxes
+    const [checkboxValues, setCheckboxValues] = useState({
+      checkbox1: false,
+      checkbox2: false,
+      checkbox3: false,
+      checkbox4: false,
+    })
+  
+
+  // handle select all checkboxs
+  const handleSelectAllChange = (e)=>{
+    const {checked} = e.target;
+    setSelectAll(checked);
+
+    const updatedCheckboxValues = {};
+    for (const key in checkboxValues){
+      updatedCheckboxValues[key] = checked;
+    }
+    setCheckboxValues(updatedCheckboxValues)
+  }
+
+
+  // handle checkbox change
+  const handleCheckboxChange = (e)=>{
+    const {name, checked} = e.target;
+    setCheckboxValues({
+      ...checkboxValues, [name]: checked,
+    })
+  }
+
+  // handle form submission
+  const handlePages = (e)=>{
+    e.preventDefault()
+   
+    console.log(`Checkbox values`, checkboxValues)
+  }
   return (
     <>
       <div className="main">
@@ -9,58 +49,55 @@ function App() {
             <p>All pages</p>
             <form>
               <label className="checkbox-container">
-                <input type="checkbox" name="all-pages" id="" />
+                <input type="checkbox"  checked={selectAll} onChange={handleSelectAllChange} />
                 <span className="checkmark"></span>
               </label>
-              
             </form>
           </div>
 
-          <div className="row">
-            <p>page 1</p>
-            <form>
-              <label className="checkbox-container">
-                <input type="checkbox" name="all-pages" id="" />
-                <span className="checkmark"></span>
-              </label>
-              
-            </form>
-          </div>
+          <form onSubmit={handlePages} >
+            <div className="row-collection">
+            <div className="row" style={checkboxValues.checkbox1 ? { textDecoration: "line-through" } : {}}>
+              <p>page 1</p>
 
-          <div className="row">
-            <p>page 2</p>
-            <form>
               <label className="checkbox-container">
-                <input type="checkbox" name="all-pages" id="" />
+                <input type="checkbox" name="checkbox1" checked={checkboxValues.checkbox1} onChange={handleCheckboxChange}  />
                 <span className="checkmark"></span>
               </label>
-              
-            </form>
-          </div>
+            </div>
 
-          <div className="row">
-            <p>page 3</p>
-            <form>
-              <label className="checkbox-container">
-                <input type="checkbox" name="all-pages" id="" />
-                <span className="checkmark"></span>
-              </label>
-              
-            </form>
-          </div>
+            <div className="row" style={checkboxValues.checkbox2 ? { textDecoration: "line-through" } : {}}>
+              <p>page 2</p>
 
-          <div className="row">
-            <p>page 4</p>
-            <form>
               <label className="checkbox-container">
-                <input type="checkbox" name="all-pages" id="" />
+                <input type="checkbox" name="checkbox2" checked={checkboxValues.checkbox2} onChange={handleCheckboxChange}/>
                 <span className="checkmark"></span>
               </label>
-              
-            </form>
-          </div>
-           
-           <button className="btn">Done</button>
+            </div>
+
+            <div className="row" style={checkboxValues.checkbox3 ? { textDecoration: "line-through" } : {}}>
+              <p>page 3</p>
+
+              <label className="checkbox-container">
+                <input type="checkbox" name="checkbox3" checked={checkboxValues.checkbox3} onChange={handleCheckboxChange} />
+                <span className="checkmark"></span>
+              </label>
+            </div>
+
+            <div className="row" style={checkboxValues.checkbox4 ? { textDecoration: "line-through" } : {}}>
+              <p>page 4</p>
+
+              <label className="checkbox-container">
+                <input type="checkbox" name="checkbox4" checked={checkboxValues.checkbox4} onChange={handleCheckboxChange} />
+                <span className="checkmark"></span>
+              </label>
+            </div>
+            </div>
+            
+            <button  type="submit" className={`btn`}>Done</button>
+          </form>
+
+         
         </div>
       </div>
     </>
